d9e9f1e605353e178d2820bccccabccee5cb8097
KMarquez17 <marquezkrodriguez24@gmail.com>
2018-10-11 16:58:45 -0600

    Menu y Home en proceso

--- src/Home/home.js
+++ src/Home/home.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react'
+import logo from '../../images/fotoHome.png'
+import FontAwesome from 'react-fontawesome'
+
 import './home.css'
 class Home extends Component {
     render() {
         return (
             <div name="home" className="container">
                 <div className="home-container">
-                    <figure>
-                        {/* <img src="" alt=""> */}
-                    </figure>
+                    <div className="home-logo">
+                        <figure>
+                            <img src={logo} />
+                        </figure>
+                    </div>
                     <div className="home-name">
                         <h1>Kevin Márquez</h1>
                         <p>Ingeniero en Computación</p>
                     </div>
-                    <div className="home-redes">
-                        <ul>
-                            <li> <a href="#">Facebook </a></li>
-                            <li> <a href="#">Instagram </a></li>
-                            <li> <a href="#">Twitter </a></li>
-                            <li> <a href="#">Google </a></li>
-                        </ul>
+                    <div className="home-redes">                        
+                            <FontAwesome
+                                className='super-crazy-colors'
+                                name='facebook-square'
+                                size='2x'
+                                // style={{ color:"white", textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+                            />
+                            <FontAwesome
+                                className='super-crazy-colors'
+                                name='instagram'
+                                size='2x'
+                                // style={{ color:"white", textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+                            />
+                            <FontAwesome
+                                className='super-crazy-colors'
+                                name='twitter-square'
+                                size='2x'
+                                // style={{ color:"white", textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+                            />
+                            <FontAwesome
+                                className='super-crazy-colors'
+                                name='google-plus-square'
+                                size='2x'
+                                // style={{ color:"white", textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+                            />
                     </div>
+                    <input type="button" name="button" value="Descargar CV"></input>
                 </div>
             </div>
         )

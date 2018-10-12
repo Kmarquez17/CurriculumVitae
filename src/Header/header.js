6ad1449b834370b2b3c96b8102953e2f66ca0b20
KMarquez17 <marquezkrodriguez24@gmail.com>
2018-10-12 16:07:24 -0600

    Responsive Header

--- src/Header/header.js
+++ src/Header/header.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react'
 import { Link } from 'react-scroll'
+import logo from '../../images/logo.png'
 import './header.css'
+import FontAwesome from 'react-fontawesome'
 
 class Header extends Component {
+    handleClick = () => {
+        console.log("Hola Kevin")
+        const menu = document.querySelector('.menu')
+        if (menu.classList.contains('is-active')) {
+            menu.classList.remove('is-active')
+        } else {
+            menu.classList.add('is-active')
+        }
+    }
     render() {
         return (
             <div className="header">
-            <div className="logo">Logo</div>
+                <div className="button-menu" onClick={this.handleClick}>
+                    <FontAwesome
+                        name='bars'
+                        size='2x'
+                    />
+                </div>
+
+                <div className="logo">
+                    <img src={logo} alt="Logo" />
+                </div>
                 <nav className="menu">
                     <ul>
-                        <li><Link activeClass="active" to="home" spy={true} smooth={true} duration={600} >Inicio</Link></li>
-                        <li><Link activeClass="active" to="about" spy={true} smooth={true} duration={500}>Datos Personal</Link></li>
-                        <li><Link activeClass="active" to="experience" spy={true} smooth={true} duration={500} >Experiencia</Link></li>
-                        <li><Link activeClass="active" to="contact" spy={true} smooth={true} duration={500} >Contacto</Link></li>
+                        <li><Link onClick={this.handleClick} activeClass="active" to="home" spy={true} smooth={true} duration={600} >Inicio</Link></li>
+                        <li><Link onClick={this.handleClick} activeClass="active" to="about" spy={true} smooth={true} duration={500}>Datos Personal</Link></li>
+                        <li><Link onClick={this.handleClick} activeClass="active" to="experience" spy={true} smooth={true} duration={500} >Experiencia</Link></li>
+                        <li><Link onClick={this.handleClick} activeClass="active" to="contact" spy={true} smooth={true} duration={500} >Contacto</Link></li>
                     </ul>
                 </nav>
             </div>
--- src/Home/home.js
+++ src/Home/home.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
-import logo from '../../images/fotoHome.png'
+import logo from '../../images/fotoPerfil.jpg'
 import FontAwesome from 'react-fontawesome'
 
 import './home.css'
+import { runInThisContext } from 'vm';
 class Home extends Component {
+    
     render() {
         return (
             <div name="home" className="container">
@@ -17,33 +19,33 @@ class Home extends Component {
                         <h1>Kevin Márquez</h1>
                         <p>Ingeniero en Computación</p>
                     </div>
-                    <div className="home-redes">                        
-                            <FontAwesome
-                                className='super-crazy-colors'
-                                name='facebook-square'
-                                size='2x'
-                                // style={{ color:"white", textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
-                            />
-                            <FontAwesome
-                                className='super-crazy-colors'
-                                name='instagram'
-                                size='2x'
-                                // style={{ color:"white", textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
-                            />
-                            <FontAwesome
-                                className='super-crazy-colors'
-                                name='twitter-square'
-                                size='2x'
-                                // style={{ color:"white", textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
-                            />
-                            <FontAwesome
-                                className='super-crazy-colors'
-                                name='google-plus-square'
-                                size='2x'
-                                // style={{ color:"white", textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
-                            />
+                    <div className="home-redes">
+                        <FontAwesome
+                            className='super-crazy-colors'
+                            name='facebook-square'
+                            size='2x'
+                        // style={{ color:"white", textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+                        />
+                        <FontAwesome
+                            className='super-crazy-colors'
+                            name='instagram'
+                            size='2x'
+                        // style={{ color:"white", textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+                        />
+                        <FontAwesome
+                            className='super-crazy-colors'
+                            name='twitter-square'
+                            size='2x'
+                        // style={{ color:"white", textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+                        />
+                        <FontAwesome
+                            className='super-crazy-colors'
+                            name='google-plus-square'
+                            size='2x'
+                        // style={{ color:"white", textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+                        />
                     </div>
-                    <input type="button" name="button" value="Descargar CV"></input>
+                    <input type="button" name="button" value="Descargar CV" onClick={this.handleClick}></input>
                 </div>
             </div>
         )

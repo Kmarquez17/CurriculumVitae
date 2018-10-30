15520dae262fbdb0d9fe672be9e635afee4070bb
KMarquez17 <marquezkrodriguez24@gmail.com>
2018-10-30 09:31:23 -0600

    Arreglando ambiente de Produccion

--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,73 @@
 const path = require('path');
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtract = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
-module.exports = (env,argv) => {
-  const plugins = [
-    new MiniCssExtractPlugin({
-      filename: "./css/[name].[hash].css",
-      chunkFilename: "[id].[chunkhash].css"
-    })
-  ]
+const basePath = __dirname;
+const distPath = 'dist';
+const indextInput = 'index.html';
+const indexOutput = 'index.html';
 
-  if (argv.mode === 'production') {
-    plugins.push(
-      new CleanWebpackPlugin(['dist'], { root: __dirname })
-    )
-  }
-
-  return {
-    entry: {
-      "app": path.resolve(__dirname, 'src/index.js'),
-    },
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'js/[name].[hash].js',
-      publicPath: path.resolve(__dirname, 'dist')+"/",
-      chunkFilename: 'js/[id].[chunkhash].js',
-    },
-    optimization: {
-      minimizer: [
-        new UglifyJsPlugin(),
-        new OptimizeCSSAssetsPlugin({})
-      ]
-    },
-    devServer: {
-      port: 5000,
-    },
-    module: {
-      rules: [
-        {
-
-          test: /\.(js|jsx)$/,
-          exclude: /(node_modules)/,
-          use: {
-            loader: 'babel-loader',
-            // options: {
-            //   presets: ['es2015', 'react', 'stage-2'],
-            // }
-          },
-        },
-        {
-          test: /\.css$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            "css-loader"
-          ]
-        },
-        {
-          test: /\.(jpg|png|gif|svg)$/,
-          use: {
-            loader: 'url-loader',
+const webpackInitConfig = {
+  mode: 'production',
+  devtool: 'none',
+  resolve: {
+    extensions: ['.js'],
+  },
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    path: path.join(basePath, distPath),
+    filename: 'js/[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+      {
+        test: /\.css/,
+        exclude: /node_modules/,
+        use: [
+          MiniCSSExtract.loader,
+          { loader: 'css-loader' },
+        ],
+      },
+      
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
             options: {
-              limit: 10000,
-              fallback: 'file-loader',
-              name: 'images/[name].[hash].[ext]',
-            }
-          }
-        },
-      ]
-    },
-    plugins
-  }
-}
+              // outputPath: 'images/',
+              // publicPath: 'images/',
+              name: 'images/[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: indexOutput,
+      template: indextInput,
+    }),
+    new MiniCSSExtract({
+      filename: 'css/[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new OptimizeCssAssetsPlugin(),
+    new UglifyJsPlugin(),
+    new CleanWebpackPlugin([distPath], {root: basePath})
+  ],
+};
+module.exports = webpackInitConfig;
